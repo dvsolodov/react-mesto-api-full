e8a18a04adf9af5ace83cf110cb3f49a7d87d326
Solodov Denis <d.v.solodov@yandex.ru>
2022-06-16 19:46:45 +1000

    fix: change baseUrl

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 class Api {
   constructor() {
-    this._baseUrl = "https://solodov.students.nomoredomains.xyz/";
+    this._baseUrl = "https://api.solodov.students.nomoredomains.xyz/";
     this._paramProfile = "/users/me/";
     this._paramAvatar = "/users/me/avatar/";
     this._paramCards = "/cards/";
