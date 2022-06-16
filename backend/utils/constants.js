8a0d5b8cff383b4b1d1bfdc3c079bc21e6e21ba7
Solodov Denis <d.v.solodov@yandex.ru>
2022-06-16 22:53:07 +1000

    fix: change allowed url

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -3,8 +3,8 @@ const passwordPattern = /^[a-zA-Z0-9]{8,}$/;
 const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const allowedCors = [
-  'http://solodov.students.nomoredomains.xyz/',
-  'https://solodov.students.nomoredomains.xyz/',
+  'http://solodov.students.nomoredomains.xyz',
+  'https://solodov.students.nomoredomains.xyz',
   'http://localhost:3000',
 ];
 
